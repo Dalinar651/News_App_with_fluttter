0a35e79a7979bcb7ae43bd3b735b74c004405ce2
Dhruv Garg <gargdhruv732@gmail.com>
2021-07-29 15:35:30 +0530

    added login persistence

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -143,20 +143,23 @@ class _loginPageState extends State<loginPage> {
                           ),
                         ),
                       ),
-                      TextButton(
-                        onPressed: LoginWithEmail,
-                        child: Container(
-                          margin: EdgeInsets.symmetric(
-                              horizontal: 40.0, vertical: 10.0),
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                              color: Color(0xff0FAAE2),
-                              borderRadius: BorderRadius.circular(50.0)),
-                          child: Center(
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.white,
+
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 40.0, vertical: 10.0),
+                        child: TextButton(
+                          onPressed: LoginWithEmail,
+                          child: Container(
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                                color: Color(0xff0FAAE2),
+                                borderRadius: BorderRadius.circular(50.0)),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
